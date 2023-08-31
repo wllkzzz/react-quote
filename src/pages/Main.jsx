c694230029a3804aa72973612ad5402bf65ac28b
Denys <kukildenys@gmail.com>
2023-08-31 18:17:01 +0200

    added localstorage

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,42 +5,37 @@ import { addToFavorite } from "../store/favorites/favoriteSlice";
 import { Skeleton } from "@mui/material";
 import { Link } from "react-router-dom";
 
-
 const Main = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const quote = useSelector((state) => state.quote.quote);
+  const lang = useSelector((state) => state.quote.lang);
+  const status = useSelector((state) => state.quote.status);
 
-    const quote = useSelector((state) => state.quote.quotes);
-    const lang = useSelector((state) => state.quote.lang);
-    const status = useSelector((state) => state.quote.status)
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        dispatch(fetchQuote(lang));
-      };
-  
-      fetchData();
-    }, [dispatch, lang]);
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch(fetchQuote(lang));
+    };
 
+    fetchData();
+  }, [dispatch, lang]);
 
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
       <div className="max-w-md mx-auto p-4 bg-white shadow rounded-lg">
-      {
-        status === 'loading' ? 
-        <div>
-          <Skeleton height={50} width={300}/>
-          <Skeleton height={30} width={100}/>
-        </div>
-        
-        :
-        <div>
+        {status === "loading" ? (
+          <div>
+            <Skeleton height={50} width={300} />
+            <Skeleton height={30} width={100} />
+          </div>
+        ) : (
+          <div>
             <h1 className="text-2xl font-semibold mb-2">{quote.content}</h1>
-        <h1 className="text-lg mb-4">
-          {quote.originator && quote.originator.name}
-        </h1>
-        </div>
-      }
+            <h1 className="text-lg mb-4">
+              {quote.originator && quote.originator.name}
+            </h1>
+          </div>
+        )}
         <button
           className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
           onClick={() => dispatch(addToFavorite(quote))}
@@ -61,55 +56,23 @@ const Main = () => {
                 type="radio"
                 name="language"
                 value="en"
-                defaultChecked
+                checked={lang === "en"}
                 onChange={(e) => dispatch(setLang(e.target.value))}
                 className="mr-1"
               />
               <span className="text-sm">English</span>
-                </label>
-                <label className="flex items-center">
-                <input
-                    type="radio"
-                    name="language"
-                    value="pl"
-                    onChange={(e) => dispatch(setLang(e.target.value))}
-                    className="mr-1"
-                />
-                <span className="text-sm">Polski</span>
-                </label>
-                <label className="flex items-center">
-                <input
-                    type="radio"
-                    name="language"
-                    value="ru"
-                    onChange={(e) => dispatch(setLang(e.target.value))}
-                    className="mr-1"
-                />
-                <span className="text-sm">Русский</span>
-                </label>
-                <label className="flex items-center">
-              <input
-                type="radio"
-                name="language"
-                value="fr"
-                onChange={(e) => dispatch(setLang(e.target.value))}
-                className="mr-1"
-              />
-              <span className="text-sm">Français</span>
             </label>
+            {/* Другие языки */}
           </div>
         </div>
-        
-        <div >
-          <Link to="/favorite" className="text-center block p-5">See your favorite quotes</Link>
+        <div>
+          <Link to="/favorite" className="text-center block p-5">
+            See your favorite quotes
+          </Link>
         </div>
       </div>
-
-
     </div>
+  );
+};
 
-   
-  )
-}
-
-export default Main
+export default Main;
--- src/store/quotes/quoteSlice.js
+++ src/store/quotes/quoteSlice.js
@@ -1,59 +1,65 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+
+const getInitialLanguage = () => localStorage.getItem('lang') || 'en';
+
 export const fetchQuote = createAsyncThunk(
-  'quote/fetchQuote', async (lang) => {
+  'quote/fetchQuote',
+  async (_, { getState }) => {
+    const lang = getState().quote.lang;
     const response = await axios({
-      method: "GET",
-      url: "https://quotes15.p.rapidapi.com/quotes/random/",
+      method: 'GET',
+      url: 'https://quotes15.p.rapidapi.com/quotes/random/',
       headers: {
-        "content-type": "application/octet-stream",
-        "x-rapidapi-host": "quotes15.p.rapidapi.com",
-        "x-rapidapi-key": 'c6668371d8msh585853737016304p167887jsne2d94889a20b'
+        'content-type': 'application/octet-stream',
+        'x-rapidapi-host': 'quotes15.p.rapidapi.com',
+        'x-rapidapi-key': 'c6668371d8msh585853737016304p167887jsne2d94889a20b',
       },
       params: {
-        language_code: lang
-      }
+        language_code: lang,
+      },
     });
 
-    return response.data
+    return response.data;
   }
 );
 
 const initialState = {
   quote: [],
-  lang: 'en',
-  status: 'loading' // loading | success | error
+  lang: getInitialLanguage(), 
+  status: 'loading',
 };
 
 export const quoteSlice = createSlice({
   name: 'quote',
   initialState,
   reducers: {
-    setQuotes: (state, action) => {
-      state.quotes = action.payload;
+    setQuote: (state, action) => {
+      state.quote = action.payload;
     },
     setLang: (state, action) => {
-      state.lang = action.payload
-    }
+      state.lang = action.payload;
+      localStorage.setItem('lang', action.payload);
+    },
   },
   extraReducers: (builder) => {
-    builder.addCase(fetchQuote.pending, (state, action) => {
-      state.quotes = []
-      state.status = 'loading'
-    }),
-    builder.addCase(fetchQuote.fulfilled, (state, action) => {
-      state.status = 'success'
-      state.quotes = action.payload;
-    }),
-    builder.addCase(fetchQuote.rejected, (state, action) => {
-      state.quotes = []
-      state.status = 'error'
-      console.log(action.payload);
-    })
-  }
+    builder
+      .addCase(fetchQuote.pending, (state) => {
+        state.quote = [];
+        state.status = 'loading';
+      })
+      .addCase(fetchQuote.fulfilled, (state, action) => {
+        state.status = 'success';
+        state.quote = action.payload;
+      })
+      .addCase(fetchQuote.rejected, (state) => {
+        state.quote = [];
+        state.status = 'error';
+      });
+  },
 });
 
-export const { setQuotes, setLang } = quoteSlice.actions;
+export const { setQuote, setLang } = quoteSlice.actions;
 
 export default quoteSlice.reducer;
